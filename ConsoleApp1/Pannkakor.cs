cb1c112da04b2008be5c3a78926641fc1c059aef
jesper.furtenbach <jesper.furtenbach@gmail.com>
2018-05-30 09:49:54 +0200

    Jesper's gren.

--- ConsoleApp1/Pannkakor.cs
+++ ConsoleApp1/Pannkakor.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hejsanhejsan");
             Console.WriteLine("Krock");
             Console.WriteLine("Blöööhp");
+            Console.WriteLine("Höörrpppusk");
         }
     }
 }
